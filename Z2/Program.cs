51b4dd100e579250f908140357c08df38ec5aa59
«Rawed» <pepe1@mail.ru>
2022-09-12 15:39:47 +0300

    save1

---
+++ Z2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции 
+// элемента в двумерном массиве, и возвращает значение этого элемента 
+// или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int [,] matrix = new int [3,4];
+Console.WriteLine("Задан массив: ");
+for(int i =0; i<matrix.GetLength(0); i++)
+{
+    for(int j =0; j<matrix.GetLength(1); j++)
+    {   
+        matrix[i,j] = new Random().Next(0,11);
+        Console.Write(matrix[i,j]+ "\t"); 
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Введите номер строки: ");
+int row  = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите номер столбца: ");
+int column  = Convert.ToInt32(Console.ReadLine()); 
+
+if (row < matrix.GetLength(0) && column < matrix.GetLength(1))
+{
+    Console.WriteLine($"Значение: {matrix[row, column]}");
+}
+else
+{
+    Console.WriteLine($"{row}{column} -> такого числа в массиве нет");
+}
---
+++ Z3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] arr = { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, {8, 4, 2, 4 } };
+Console.WriteLine("Задан массив: ");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        
+        Console.Write($"{arr[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Среднее арифметическое каждого столбца: ");
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    sum = sum / arr.GetLength(0);
+    Console.Write($" {sum.ToString("0.00")}");
+}
